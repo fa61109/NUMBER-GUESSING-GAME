277250f2e27151514556334cec464e28095c0a6c
fa61109 <fa61109@gmail.com>
2024-03-27 01:53:41 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "userguessednumber",
+        messeage: "please Guess a Number Between 1-10",
+    },
+]);
+if (answer.userguessednumber === randomNumber) {
+    console.log("Congratulations! you guessed right number");
+}
+else {
+    console.log("You guessed wrong number");
+}
